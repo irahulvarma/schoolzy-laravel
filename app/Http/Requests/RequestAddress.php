d08e1d3754ef95816373ad5c0469f3cc874d03c4
rahul.varma <rahul.spnd@gmail.com>
2023-01-27 20:26:47 +0530

    adding request form for address

---
+++ app/Http/Requests/RequestAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class RequestAddress extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::user()->role == 'admin';
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'flat_no' => 'required|max:255',
+        ];
+    }
+}
